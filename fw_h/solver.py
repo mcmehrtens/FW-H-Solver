Implemented visualization methods and fixed bug in observer pressure calculation

fw_h/solver.py
@@ -1,14 +1,17 @@
 """Implement the FW-H equation solver using Farassat's Formula 1A."""
 
+import datetime
 import logging
 import math
+from pathlib import Path
 
 import numpy as np
+import yaml
 from numpy.typing import NDArray
 
 from fw_h.config import ConfigSchema
 from fw_h.geometry import Surface
-from fw_h.source import SourceData
+from fw_h.source import NumericalSourceParams, SourceData
 
 logger = logging.getLogger(__name__)
 
@@ -176,30 +179,24 @@ class Solver:
             self.source.surface.x
         )
         fw_h_surface_n = np.sqrt(len(self.source.surface.x) / 6)
-        cell_area = (2 * fw_h_surface_r / fw_h_surface_n) ** 2
+        cell_area = (fw_h_surface_r / (fw_h_surface_n - 1)) ** 2
 
         logger.debug("Calculating the thickness term...")
-        p_t_tau = (
-            cell_area
-            / (4 * np.pi)
-            * (self.config.solver.constants.rho_0 * self.v_n_dot / self.r)
+        p_t_tau = cell_area * (
+            self.config.solver.constants.rho_0 * self.v_n_dot / self.r
         )
 
         logger.debug("Calculating the loading term...")
-        p_l_tau = (
-            cell_area
-            / (4 * np.pi)
-            * (
-                (
-                    self.p_dot
-                    * self.cos_theta
-                    / (self.config.solver.constants.c_0 * self.r)
-                )
-                + (self.source.pressure * self.cos_theta / self.r**2)
+        p_l_tau = cell_area * (
+            (
+                self.p_dot
+                * self.cos_theta
+                / (self.config.solver.constants.c_0 * self.r)
             )
+            + (self.source.pressure * self.cos_theta / self.r**2)
         )
 
-        p_tau = p_t_tau + p_l_tau
+        p_tau = (p_t_tau + p_l_tau) / (4 * np.pi)
 
         logger.info("Converting pressure from source time to observer time...")
         logger.debug("Preallocating pressure array...")
@@ -213,3 +210,60 @@ class Solver:
 
         self._convert_source_to_observer_pressure(p_tau, retarded_time)
         logger.info("Finished converting pressure to observer time.")
+
+    def validate(self) -> None:
+        """Calculate observer pressure using analytical functions."""
+        logger.info(
+            "Calculating validation pressure over observer time domain..."
+        )
+        data = NumericalSourceParams(
+            x=self.observer_surface.x[:, np.newaxis],
+            y=self.observer_surface.y[:, np.newaxis],
+            z=self.observer_surface.z[:, np.newaxis],
+            x_0=self.source.config.source.point.x,
+            y_0=self.source.config.source.point.y,
+            z_0=self.source.config.source.point.z,
+            t=self.time_domain[np.newaxis, :],
+            amplitude=self.source.config.source.amplitude,
+            omega=self.source.config.source.frequency,
+            c_0=self.source.config.source.constants.c_0,
+            rho_0=self.source.config.source.constants.rho_0,
+        )
+        self.p_analytical = self.source.analytical_source.pressure(data)
+
+    def write(self, *exclude: tuple[str, ...]) -> None:
+        """Write relevant data to a NumPy .npz archive.
+
+        Parameters
+        ----------
+        exclude
+            List of arrays to exclude from writing to the file.
+
+        """
+        logger.info("Writing analytical source data to file...")
+        arrays = {
+            "time_domain": self.time_domain,
+            "p": self.p,
+            "p_analytical": self.p_analytical,
+        }
+        arrays = {k: v for k, v in arrays.items() if k not in exclude}
+
+        output_dir = self.config.global_config.output.output_dir
+
+        timestamp = datetime.datetime.now(tz=datetime.UTC).strftime(
+            self.config.global_config.output.output_file_timestamp
+        )
+
+        config_path = Path(output_dir) / f"{timestamp}-fw-h-config.yaml"
+        with config_path.open("w") as file:
+            logger.info("Writing config to %s...", config_path)
+            yaml.dump(
+                self.config.model_dump(warnings="error"),
+                file,
+                default_flow_style=False,
+            )
+
+        data_path = Path(output_dir) / f"{timestamp}-fw-h.npz"
+        logger.info("Writing data to %s...", data_path)
+        logger.info("This may take a while...")
+        np.savez_compressed(data_path, **arrays)

fw_h/source.py
@@ -457,6 +457,12 @@ class SourceData:
             rho_0=self.config.source.constants.rho_0,
         )
 
+        self.velocity_potential = np.array([[0]], dtype=np.float64)
+        self.pressure = np.array([[0]], dtype=np.float64)
+        self.velocity_x = np.array([[0]], dtype=np.float64)
+        self.velocity_y = np.array([[0]], dtype=np.float64)
+        self.velocity_z = np.array([[0]], dtype=np.float64)
+
     def _mesh(self) -> None:
         """Mesh the surfaces and discretize the time domain.
 
@@ -490,11 +496,11 @@ class SourceData:
         source = self.config.source.description
         match source:
             case SourceType.MONOPOLE:
-                self._analytical_source = MonopoleSource(
+                self.analytical_source = MonopoleSource(
                     self._source_shape_function
                 )
             case SourceType.DIPOLE:
-                self._analytical_source = DipoleSource(
+                self.analytical_source = DipoleSource(
                     self._source_shape_function
                 )
             case _:
@@ -504,21 +510,21 @@ class SourceData:
     def calculate_velocity_potential(self) -> None:
         """Calculate the velocity potential."""
         logger.info("Calculating velocity potential...")
-        self.velocity_potential = self._analytical_source.velocity_potential(
+        self.velocity_potential = self.analytical_source.velocity_potential(
             self.data
         ).T
 
     def calculate_pressure(self) -> None:
         """Calculate the pressure."""
         logger.info("Calculating pressure...")
-        self.pressure = self._analytical_source.pressure(self.data).T
+        self.pressure = self.analytical_source.pressure(self.data).T
 
     def calculate_velocity(self) -> None:
         """Calculate the velocity."""
         logger.info("Calculating velocity...")
-        self.velocity_x = self._analytical_source.velocity_x(self.data).T
-        self.velocity_y = self._analytical_source.velocity_y(self.data).T
-        self.velocity_z = self._analytical_source.velocity_z(self.data).T
+        self.velocity_x = self.analytical_source.velocity_x(self.data).T
+        self.velocity_y = self.analytical_source.velocity_y(self.data).T
+        self.velocity_z = self.analytical_source.velocity_z(self.data).T
 
     def write(self, *exclude: tuple[str, ...]) -> None:
         """Write relevant data to a NumPy .npz archive.
@@ -546,10 +552,10 @@ class SourceData:
         }
         arrays = {k: v for k, v in arrays.items() if k not in exclude}
 
-        output_dir = self.config.solver.output.output_dir
+        output_dir = self.config.global_config.output.output_dir
 
         timestamp = datetime.datetime.now(tz=datetime.UTC).strftime(
-            self.config.solver.output.output_file_timestamp
+            self.config.global_config.output.output_file_timestamp
         )
 
         config_path = Path(output_dir) / f"{timestamp}-fw-h-config.yaml"
@@ -581,9 +587,9 @@ class SourceData:
             logger.debug("Loading surface mesh...")
             normals = (
                 Normals(
-                    source_data["fw_h_surface_n_x"],
-                    source_data["fw_h_surface_n_y"],
-                    source_data["fw_h_surface_n_z"],
+                    source_data["surface_n_x"],
+                    source_data["surface_n_y"],
+                    source_data["surface_n_z"],
                 )
                 if "surface_n_x" in source_data
                 and "surface_n_y" in source_data
@@ -592,9 +598,9 @@ class SourceData:
             )
 
             self.surface = Surface(
-                source_data["fw_h_surface_x"],
-                source_data["fw_h_surface_y"],
-                source_data["fw_h_surface_z"],
+                source_data["surface_x"],
+                source_data["surface_y"],
+                source_data["surface_z"],
                 normals,
             )
 